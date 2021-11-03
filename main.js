c46cce8b55b4107b795b7fe8b6dd44e27aea192d
smediums <smediums.me@gmail.com>
2021-11-04 01:08:33 +0300

    Add set word count goal input feild and the its code to output put its value to the div below it. Also added code to decrease word count goal as user types.

--- main.js
+++ main.js
@@ -2,16 +2,22 @@ const textArea = document.getElementById('inputTextArea');
 const charCount = document.getElementById('characterCount');
 const wordCount = document.getElementById('wordCount');
 const setGoal = document.getElementById('wordsAmount');
+const set = document.getElementById('set');
+const tillGoal = document.querySelector('.wordsTillGoal');
 
-console.log(setGoal)
 //
 textArea.addEventListener('input', () => {
+    let curWordCount = parseInt(wordCount.textContent);
+    let curWordsTillGoal = parseInt(setGoal.value);
     //Number of Characters
     charCount.textContent = textArea.value.length;
 
     //Create array By Spaces 
     let allText = textArea.value.trim();
     wordCount.textContent = allText.split(/\s+/).filter((el) => el).length;
+
+    tillGoal.innerHTML = `${curWordsTillGoal - curWordCount} words until reach goal`
+   
 });
 
 setGoal.addEventListener('input', () => {
@@ -21,6 +27,16 @@ setGoal.addEventListener('input', () => {
     if(!onlyNumbers.test(num)){
         setGoal.value = '';
     }
+})
 
-    console.log(num)
+set.addEventListener('click', () => {
+
+    if(setGoal.value == '' ){
+        tillGoal.innerHTML = `0 words until you reach your goal`
+
+    }else{
+        tillGoal.innerHTML = `${setGoal.value} words until you reach your goal`
+    console.log(parseInt(wordCount.textContent))
+    }
+    
 })
