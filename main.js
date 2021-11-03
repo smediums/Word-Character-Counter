909e3f4dd2b4c2e6d44c2c001a50c98caffcc02e
smediums <smediums.me@gmail.com>
2021-11-04 00:02:08 +0300

    Centered content

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 const textArea = document.getElementById('inputTextArea');
 const charCount = document.getElementById('characterCount');
 const wordCount = document.getElementById('wordCount');
+const setGoal = document.getElementById('wordsAmount');
 
-
+console.log(setGoal)
 //
 textArea.addEventListener('input', () => {
     //Number of Characters
@@ -11,6 +12,15 @@ textArea.addEventListener('input', () => {
     //Create array By Spaces 
     let allText = textArea.value.trim();
     wordCount.textContent = allText.split(/\s+/).filter((el) => el).length;
-
-
 });
+
+// setGoal.addEventListener('input', () => {
+//     const num = parseInt(setGoal.value);
+//     const onlyNumbers = /[0-9]|\./;
+
+//     if(!onlyNumbers.test(num)){
+//         setGoal.value = '';
+//     }
+
+//     console.log(num)
+// })
